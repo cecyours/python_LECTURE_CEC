e0b1bc276f631f6cf325fe3b748114962def6981
GK <gpkmyworld@gmail.com>
2021-10-05 10:56:44 +0530

    05/10/2021⠀⎦⎡⠀10:56⠀⎦⎢⠀Tuesday

---
+++ CH02_P08_tuple.py
@@ -0,0 +1,20 @@
+
+t = (1,2,3,4,5,6,7,8,9,0)
+
+print(t)
+print("-------------------")
+
+print("indexing : ",t[4])
+print("slicing  : ",t[2:5:2])
+
+print("-------------------")
+## if 
+if 34 not in t:
+	print("Hello")
+else:
+	print("Bye")
+
+print("-------------------")
+
+for i in t:
+	print("====> ",i)
