378101e0f8d98d4e94a4eb28ac9aa519d968b626
GkHell <gpkmyworld@gmail.com>
2021-10-17 10:53:04 +0530

    17/10/2021⠀⎦⎡⠀10:53⠀⎦⎢⠀Sunday

---
+++ CH04_P05_range_xrange.py
@@ -0,0 +1,16 @@
+
+
+x = range(10,21)
+
+print(type(x))
+print(x[2:9]) #slicing
+print(x[5]) #indexing
+print("------------------------")
+
+# for i in xrange(0,10):  ## NOTE : only in python2
+# 	print("HELLO")
+
+
+
+
+ 
---
+++ CH04_P06_forLoop.py
@@ -0,0 +1,12 @@
+
+
+# [start:end:step]
+for i in range(1,11,2):
+	print(i,'=',i**2)
+
+##
+print("-------------")
+x = [10,20,30,40,50,60,60,40,60,20,60,12]
+data = 
+
+# print("{:2} x {:2} = {}"format(a,i,a*i))
