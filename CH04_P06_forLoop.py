9b7432eea8a1d4a99b9ca573b68666468012d01d
GkHell <gpkmyworld@gmail.com>
2021-10-17 11:02:05 +0530

    17/10/2021⠀⎦⎡⠀11:02⠀⎦⎢⠀Sunday

--- CH04_P06_forLoop.py
+++ CH04_P06_forLoop.py
@@ -7,6 +7,13 @@ for i in range(1,11,2):
 ##
 print("-------------")
 x = [10,20,30,40,50,60,60,40,60,20,60,12]
-data = 
+data = []
 
-# print("{:2} x {:2} = {}"format(a,i,a*i))
+
+for i in x:
+	if i not in data:
+		data.append(i)
+		print(i) 
+
+print(x)
+print(data)
---
+++ CH04_P07_while.py
@@ -0,0 +1,8 @@
+
+i = 9;
+while i<=100:
+	i+=1 #i = i+1
+
+	if i%2!=0:
+		continue
+	print(i,end=", ")
