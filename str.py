0aeff976dc4716bf46e33f6bee7be11088e57adb
GKHell <78089716+GKHell@users.noreply.github.com>
2021-10-26 18:33:42 +0530

    no date :) 26/10/2021

---
+++ str.py
@@ -0,0 +1,33 @@
+
+s = "hi me!!"
+
+x = s.capitalize()
+print(x)
+
+##print(s,x,sep=" ------------ ")
+
+x = s.casefold() # convert into lowercase
+print(x)
+
+x = s.center(20,'-')
+print(x)
+
+s = '123'
+x = s.zfill(5)
+print(x)
+
+s = "Hello jay, im new jay."
+x = s.count('im')
+print(x)
+
+
+# TASK
+'''
+1. capital first latter name entered by user
+2. find how many there are 'a' in user's line.
+
+
+
+
+
+'''
