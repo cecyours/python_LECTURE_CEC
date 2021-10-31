df77b6c66dc19c97e7e195145ca093d11dee6054
GkHell <gpkmyworld@gmail.com>
2021-10-31 11:05:26 +0530

    31/10/2021⠀⎦⎡⠀11:05⠀⎦⎢⠀Sunday

--- TASK/DS_P05_singlyLINKEDLIST.c
+++ TASK/DS_P05_singlyLINKEDLIST.c
@@ -9,6 +9,27 @@ struct Node{
 
 void insertFront()
 {
+    struct Node *newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    printf("enter the data : ");
+    scanf("%d",&newNode->data);
+    
+   printf("insertion done...\n");
+
+    if(!head)
+    {
+        head = newNode;
+        return;
+    }
+
+   newNode->next = head;
+   head = newNode;
+}
+
+void insertEnd()
+{
     struct Node *temp,*newNode;
 
     newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -22,9 +43,65 @@ void insertFront()
         return;
     }
 
-   newNode->next = head;
-   head = newNode;
+    temp = head;
+
+    while(temp->next)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+   printf("insertion done...\n");
+
 }
+ 
+void insertSort()
+{
+      struct Node *temp,*newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    printf("enter the data : ");
+    scanf("%d",&newNode->data);
+    
+    printf("insertion done...\n");
+
+    if(!head)
+    {
+           head = newNode; // newNode is head is there is neither a Node nor newNode->data is smaller than head.
+        return;
+    }
+
+    if(!head->next) // if second Node is NULL
+    {
+        if(newNode->data > head->data)
+            {
+                head->next = newNode;
+                return;
+            }
+            newNode->next = head;
+            head = newNode; // newNode is head is there is neither a Node nor newNode->data is smaller than head.
+
+            return;
+    }
+
+    temp = head;
+    while(temp->next)
+    {
+            if(newNode->data > temp->data && newNode->data < temp->next->data)
+            {
+                newNode->next = temp->next;
+                temp->next = newNode;
+                return;
+            }
+    temp = temp->next;
+
+    }
+    temp->next = newNode;
+
+}
+
+
 void display()
 {
     struct Node *temp;
@@ -35,6 +112,7 @@ void display()
     }
     temp = head;
 
+    printf("linked list : ");
     while(temp->next)
     {
         printf("%d -> ",temp->data);
@@ -42,19 +120,15 @@ void display()
     }
     printf("%d.",temp->data);
 
-
 }
 
 int main()
 {
-    insertFront();
-    insertFront();
-    insertFront();
-    insertFront();
-    insertFront();
-    insertFront();
-    insertFront();
+    
 
-
+    insertFront();
+    insertFront();
+    insertEnd();   
+    insertSort();  
     display();
 }
