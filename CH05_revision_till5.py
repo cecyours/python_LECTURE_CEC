5ac719454df0d2962efeea0086be82545ee3c431
GkHell <gpkmyworld@gmail.com>
2021-11-11 19:24:53 +0530

    11/11/2021⠀⎦⎡⠀19:24⠀⎦⎢⠀Thursday

---
+++ CH05_revision_till5.py
@@ -0,0 +1,19 @@
+
+code = []
+
+n = int(input("Enter size : "));
+
+for i in range(n):
+    x = int(input("Enter element : "))
+    code.append(x);
+
+
+
+counter = 0;
+
+for i in code:
+    print(i)
+    if i<0:
+        counter+=1; # counter = counter +1
+
+print(code,counter);
