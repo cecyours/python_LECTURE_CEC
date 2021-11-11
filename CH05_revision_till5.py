338d7d1d13332d62071e2867e1db41d763a29cca
GkHell <gpkmyworld@gmail.com>
2021-11-11 20:25:20 +0530

    11/11/2021⠀⎦⎡⠀20:25⠀⎦⎢⠀Thursday

--- CH05_revision_till5.py
+++ CH05_revision_till5.py
@@ -7,13 +7,11 @@ for i in range(n):
     x = int(input("Enter element : "))
     code.append(x);
 
-
-
 counter = 0;
 
 for i in code:
-    print(i)
+    print("{:2}".format(i))
     if i<0:
         counter+=1; # counter = counter +1
-
+     
 print(code,counter);
---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+
+class Hey{
+
+    public static void main(String hell[])
+    {
+        System.out.println("Hello Jay");
+        System.out.println("Hell of COde");
+        System.out.print("1234\t5678");
+
+    }
+}
