7c247f0d60a2a891d64a03e069ea0d4a7967dc6f
GkHell <gpkmyworld@gmail.com>
2021-11-15 13:14:07 +0530

    15/11/2021⠀⎦⎡⠀13:14⠀⎦⎢⠀Monday

--- TASK/DS_15/P03_orders.c
+++ TASK/DS_15/P03_orders.c
@@ -1,17 +1,14 @@
-// C program for different tree traversals
+ 
 #include <stdio.h>
 #include <stdlib.h>
-
-/* A binary tree node has data, pointer to left child
-and a pointer to right child */
+ 
 struct node {
 	int data;
 	struct node* left;
 	struct node* right;
 };
 
-/* Helper function that allocates a new node with the
-given data and NULL left and right pointers. */
+ 
 struct node* newNode(int data)
 {
 	struct node* node
@@ -23,56 +20,52 @@ struct node* newNode(int data)
 	return (node);
 }
 
-/* Given a binary tree, print its nodes according to the
-"bottom-up" postorder traversal. */
+ 
 void printPostorder(struct node* node)
 {
 	if (node == NULL)
 		return;
 
-	// first recur on left subtree
+	 
 	printPostorder(node->left);
-
-	// then recur on right subtree
+ 
 	printPostorder(node->right);
 
-	// now deal with the node
+	 
 	printf("%d ", node->data);
 }
-
-/* Given a binary tree, print its nodes in inorder*/
+ 
 void printInorder(struct node* node)
 {
 	if (node == NULL)
 		return;
 
-	/* first recur on left child */
+ 
 	printInorder(node->left);
 
-	/* then print the data of node */
+	 
 	printf("%d ", node->data);
 
-	/* now recur on right child */
+	 
 	printInorder(node->right);
 }
 
-/* Given a binary tree, print its nodes in preorder*/
+ 
 void printPreorder(struct node* node)
 {
 	if (node == NULL)
 		return;
 
-	/* first print data of node */
+	 
 	printf("%d ", node->data);
 
-	/* then recur on left subtree */
+ 
 	printPreorder(node->left);
-
-	/* now recur on right subtree */
+ 
 	printPreorder(node->right);
 }
 
-/* Driver program to test above functions*/
+ 
 int main()
 {
 	struct node* root = newNode(1);
